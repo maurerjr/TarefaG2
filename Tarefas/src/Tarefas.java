caded62e069503d3f23a133251639d63aef5eb56
maurerjr <willy_maurerjr@hotmail.com>
2015-12-08 11:41:00 -0200

    Com CRUD

--- Tarefas/src/Tarefas.java
+++ Tarefas/src/Tarefas.java
@@ -2,6 +2,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tarefas {
@@ -53,18 +54,23 @@ public class Tarefas {
 		this.urgencia = urgencia;
 	}
 	
-	public void ler (){		
+	public void ler (){
+		Tarefas t = new Tarefas();
 		System.out.println("Nome: ");
 		String nome=tc.next();
 		
+		
+		
 		System.out.println("Data de inicio: ");
-		String dInicio=tc.next();
+		String inicio=tc.next();
+		String dInicio = t.validaData(inicio);
 		LocalDate dI = LocalDate.parse(dInicio);
 		
 		
 		
 		System.out.println("Data de finalização? ");
-		String dFinal=tc.next();
+		String final2=tc.next();
+		String dFinal=t.validaData(final2);
 		LocalDate dF = LocalDate.parse(dFinal);
 		
 		
@@ -97,20 +103,70 @@ public class Tarefas {
 		
 	}
 	
+	public String validaData(String data){
+		String dia=data.substring(0,2);
+		String mes=data.substring(2,4);
+		String  ano=data.substring(4,8);
+		
+		
+		System.out.println(ano+"-"+mes+"-"+dia);
+		return ano+"-"+mes+"-"+dia;
+		
+	}
+	
+	
+	public boolean mostrarTudo(int idDisciplina){
+		Mysql m = new Mysql();
+		String sql ="SELECT * FROM tarefas where idDisciplinas='"+idDisciplina+"'";
+		try {
+			 
+			PreparedStatement comandoSQl = (PreparedStatement) m.conexao.prepareStatement(sql);
+			
+			 
+			 
+			 
+			 
+			 ResultSet rs =comandoSQl.executeQuery();
+			 while (rs.next()) {
+				 int id = rs.getInt("idTarefa");
+				 String nome = rs.getString("nome");
+				 String dInicio = rs.getString("dInicio");
+				 String dFim = rs.getString("dFim");
+				 int urgencia = rs.getInt("urgencia");
+				 int pronta = rs.getInt("pronta");
+				 
+				 
+				 
+				
+				 System.out.println("\n Tarefas dessa disciplina: \n \nid: "+id+" nome: "+nome+" Data de inicio: "+dInicio+" Data de entrega: "+dFim+" Urgencia: "+urgencia+"Pronta(1-sim 0-nao? "+pronta);
+				 
+			
+				 
+				 
+			 }
+		} catch (SQLException e) {
+			// TODO: handle exception
+			m.msg = e.getMessage();
+			return false;
+		}
+		 return false;
+	}
+	
 
 	
-	public boolean salvar(){
+	public boolean salvar(int id){
 		Mysql m = new Mysql();
 		
 	
 	 try {		
-			String sql = "INSERT INTO tarefas (nome, dInicio, dFim, urgencia, pronta)VALUES(?,?,?,?,?);";
+			String sql = "INSERT INTO tarefas (nome, dInicio, dFim, urgencia, pronta, idDisciplina)VALUES(?,?,?,?,?, ?);";
 			PreparedStatement comandoSQl = (PreparedStatement) m.conexao.prepareStatement(sql);
 			comandoSQl.setString(1, this.nome);
 			comandoSQl.setString(2, this.dInicio);
 			comandoSQl.setString(3, this.dFim);
 			comandoSQl.setInt(4, this.urgencia);
 			comandoSQl.setInt(5, this.pronta);
+			comandoSQl.setInt(6, id);
 
 
 			
@@ -128,6 +184,77 @@ public class Tarefas {
 
 	 
  }
+	
+
+		
+	
+	public boolean atualizar(int id){
+		
+		
+		Mysql m = new Mysql();
+			
+		
+		
+		 try {		
+				String sql = "UPDATE tarefas SET nome = ?, dInicio=?, dFim=?, urgencia=?, pronta=? where idTarefas=?";
+				PreparedStatement comandoSQl = (PreparedStatement) m.conexao.prepareStatement(sql);
+				comandoSQl.setString(1, this.nome);
+				comandoSQl.setString(2, this.dInicio);
+				comandoSQl.setString(3, this.dFim);
+				comandoSQl.setInt(4, this.urgencia);
+				comandoSQl.setInt(5, this.pronta);
+				comandoSQl.setInt(6, id);
+				
+
+
+				
+				comandoSQl.execute();
+				
+				
+				comandoSQl.close();
+				return true;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				m.msg =e.getMessage();
+				return false;
+			}	
+
+		 
+		
+		
+	}
+	
+	
+	
+	
+public boolean remover(int id){
+		
+		
+		Mysql m = new Mysql();
+			
+		
+		
+		 try {		
+				String sql = "DELETE FROM tarefas WHERE idtarefas = ?";
+				PreparedStatement comandoSQl = (PreparedStatement) m.conexao.prepareStatement(sql);
+				comandoSQl.setInt(1, id);
+
+				
+				comandoSQl.execute();
+				
+				
+				comandoSQl.close();
+				return true;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				m.msg =e.getMessage();
+				return false;
+			}	
+}
+
+	
 	public boolean mostrar(int funcao){
 		 Mysql m = new Mysql();
 		 String sql = null;
@@ -139,6 +266,10 @@ public class Tarefas {
 		 case 2: sql= "SELECT * FROM tarefas WHERE CURRENT_DATE < dFim";
 		 break;
 		 case 3: sql="SELECT * FROM tarefas ORDER BY urgencia ASC";
+		 break;
+		 case 4: sql="SELECT * FROM tarefas";
+		 break;
+
 		 }
 		 
 		 try {
@@ -170,7 +301,7 @@ public class Tarefas {
 	 */
 	@Override
 	public String toString() {
-		return "Cadastro [nome=" + nome + ", dInicio=" + dInicio + ", dFim=" + dFim + ", pronta=" + pronta
+		return "Tarefas [nome=" + nome + ", dInicio=" + dInicio + ", dFim=" + dFim + ", pronta=" + pronta
 				+ ", urgencia=" + urgencia + ", id=" + id + "]";
 	}
 	
